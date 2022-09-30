016d18a2f48730b9e652a5a4e1c7215f8a550b53
Travis <travis.m.bruce@gmail.com>
2022-09-29 19:39:08 -0700

    GraphQL setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config("");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
